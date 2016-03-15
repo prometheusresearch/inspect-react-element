bca0c10030339f4720a32b5d61be4a78abafb1bb
Andrey Popp <8mayday@gmail.com>
2016-03-15 12:36:42 +0300

    Add tests for displayName preference.

--- test/inspectReactElement-test.js
+++ test/inspectReactElement-test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var tap = require('tap');
  
 tap.test('inspectReactElement produces desired output', function (t) {
@@ -6,4 +8,16 @@ tap.test('inspectReactElement produces desired output', function (t) {
   var React = require('react');
 
   t.equal(inspectReactElement(React.createElement('div')), '<div />');
+});
+
+tap.test('inspectReactElement picks up .displayName before .name', function (t) {
+  t.plan(1);
+  var inspectReactElement = require('../');
+  var React = require('react');
+
+  class Custom extends React.Component {
+  }
+  Custom.displayName = 'Super';
+
+  t.equal(inspectReactElement(React.createElement(Custom)), '<Super />');
 });
